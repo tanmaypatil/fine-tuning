fd368008b522a463ddd30b439f2fe9ceb97f7ac7
Tanmay Patil <tany.patil77@gmail.com>
2024-06-03 00:45:07 +0530

    lora training

---
+++ config_train.py
@@ -0,0 +1,6 @@
+
+config_obj = {
+  "model_name": "distilbert/distilgpt2"
+}
+
+print(config_obj['model_name'])
---
+++ util_functions.py
@@ -0,0 +1,34 @@
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
+)
+
+# We still need to concatenate our sequences
+# and split them into shorter chunks to ease
+# minimal RAM usage
+block_size = 128
+
+
+def get_tokenizer(model_name):
+  tokenizer = AutoTokenizer.from_pretrained(model_name)
+  return tokenizer
+
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
